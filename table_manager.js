62db92feb1e15aa276a603410bb66c3ee2115721
Yannick Ganser <ganseryannick@gmail.com>
2026-07-31 02:57:07 +0200

    erster commit

---
+++ table_manager.js
@@ -0,0 +1,54 @@
+const tbody = document.getElementById("gameBody");
+var counter=0;
+function init() {
+    const limit =5;
+
+    for (let i = 0; i < 10; i++) {
+
+        const row = document.createElement("tr")
+        let str ="<td contenteditable=\"true\" class =" + i.toString() +"></td> \n"
+
+        for (let j = 0; j < limit; j++) {
+            const cell = document.createElement("td");
+
+            cell.contentEditable = true;
+
+            cell.className = `col-${j}`;  
+
+            row.appendChild(cell);
+        }
+
+        tbody.appendChild(row);
+    }
+}
+
+init()
+
+function hide_colum(collum){
+    const elements = document.querySelectorAll(`.col-${collum}`);
+    elements.forEach(element => {
+    element.style.display = "none";
+});
+
+return "done"
+}
+
+function show_column(column) {
+        const elements = document.querySelectorAll(`.col-${column}`);
+        elements.forEach(element => {
+            element.style.display = "";
+        });
+    
+}
+function hide_dynamik() {
+    if (counter < 5) {
+        hide_colum(counter);
+        counter++;
+    }
+}
+function show_dynamik() {
+    if (counter > 0) {
+        counter--;
+        show_column(counter);
+    }
+}
